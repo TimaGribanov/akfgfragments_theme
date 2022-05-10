30e41dcb5a22de5e76970852af89c109fbc837d1
Timofey Gribanov <48593815+TimaGribanov@users.noreply.github.com>
2022-05-10 12:49:56 +0300

    Sidebar a bit

--- wp-content/themes/akfgfragments/sidebar.php
+++ wp-content/themes/akfgfragments/sidebar.php
@@ -1,7 +1,37 @@
+<script type="text/javascript">
+    //Scroll through socials
+    (function($) {
+
+
+
+    })( jQuery );
+</script>
+
 <div id="main-sidebar" class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-    <div id="main-discord" class="text-center side-block">
+    <div id="main-discord" class="main-side-block text-center border border-light border-2 rounded-2">
         <h4>Join our Discord server!</h3>
         <p>We have interesting Asian Kung-Fu Generation related discussions there, and more!</p>
         <button type="button" class="btn btn-discord"><i class="bi bi-discord"></i><a href="https://discord.gg/mQJ4TcjM3h">Join</a></button>
+    </div>
+
+    <div id="main-socials-block" class="main-side-block text-center border border-light border-2 rounded-2">
+        <div id="main-social-1" class="main-socials">
+            <a class="twitter-timeline" data-width="300" data-height="400" data-dnt="true" href="https://twitter.com/AKG_information?ref_src=twsrc%5Etfw">Tweets by AKG_information</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+        <div id="main-social-2" class="main-socials main-socials-plus">
+            <a class="twitter-timeline" data-width="300" data-height="400" data-dnt="true" href="https://twitter.com/gotch_akg?ref_src=twsrc%5Etfw">Tweets by gotch_akg</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+        <div id="main-social-3" class="main-socials main-socials-plus">
+            <a class="twitter-timeline" data-width="300" data-height="400" data-dnt="true" href="https://twitter.com/kiyoshiakg?ref_src=twsrc%5Etfw">Tweets by kiyoshiakg</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+        <div>
+            <i id="main-social-1-btn" class="bi bi-dot"></i>
+            <i id="main-social-2-btn" class="bi bi-dot"></i>
+            <i id="main-social-3-btn" class="bi bi-dot"></i>
+        </div>
+    </div>
+
+    <div id="main-ajikan-project" class="main-side-block text-center border border-light border-2 rounded-2">
+        <a href="https://ajikanproject.com" target="_blank"><img src="wp-content/themes/akfgfragments/assets/img/ajikan_project_logo.png" width="300px" height="300px"/></a>
     </div>
 </div>
