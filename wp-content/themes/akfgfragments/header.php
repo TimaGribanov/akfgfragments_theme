762f487b2667c0a0444639c74e208ae58bbd1a69
root <root@DELL.localdomain>
2022-05-13 10:24:29 +0300

    Changed header

--- wp-content/themes/akfgfragments/header.php
+++ wp-content/themes/akfgfragments/header.php
@@ -3,7 +3,8 @@
 
 <head>
     <?php
-        $url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        
         $slug = parse_url($url, PHP_URL_PATH);
         $slug = str_replace('/', '', $slug);
         $slug = str_replace('_', ' ', $slug);
@@ -15,21 +16,25 @@
         $title_parsed = str_replace('/', '', $title); //Delete slashes if they exist
         $title_parsed = str_replace('_', ' ', $title_parsed); //Delete underscores if they exist
         $title_parsed = str_replace('%27', '\'', $title_parsed); //Change %27 to a single quote
-        $title_parsed = str_replace('%26', '&', $title_parsed); //Cahnge %26 to an ampersand
-        $title_parsed = str_replace('%23', '#', $title_parsed); //Cahnge %26 to a number sign
-        $title_parsed = str_replace('%3F', '?', $title_parsed); //Cahnge %26 to a question mark
+        $title_parsed = str_replace('%26', '&', $title_parsed); //Change %26 to an ampersand
+        $title_parsed = str_replace('%23', '#', $title_parsed); //Change %26 to a number sign
+        $title_parsed = str_replace('%3F', '?', $title_parsed); //Change %26 to a question mark
         $title_parsed = ucwords($title_parsed); //Capitalise each word
     ?>
     <title>
         <?php
-            if (!$title_parsed) {
-                if (!$slug) {
-                    echo "Home";
-                } else {
-                    echo($slug);
-                }
+            if(!empty($post->post_title) && strcmp($post->post_title, "Release") !== 0 && strcmp($post->post_title, "Song") !== 0 && !(parse_url($url, PHP_URL_PATH) == "/")) {
+                    echo get_the_title();
             } else {
-                echo($title_parsed);
+                if (!$title_parsed) {
+                    if (!$slug) {
+                        echo "Home";
+                    } else {
+                        echo($slug);
+                    }
+                } else {
+                    echo($title_parsed);
+                }
             }
         ?>
     </title>
