d7711154d3aad913062010099902773737883dc3
TimaGribanov <48593815+TimaGribanov@users.noreply.github.com>
2023-01-31 15:04:00 +0500

    Fixed grid of the music videos list

--- wp-content/themes/akfgfragments/templates/music-videos.php
+++ wp-content/themes/akfgfragments/templates/music-videos.php
@@ -1,8 +1,8 @@
-<?php /* Template Name: Akfgfragments Music Videos */?>
+<?php /* Template Name: Akfgfragments Music Videos' List */?>
 
 <?php get_header(); ?>
 
-<?php require( get_theme_root() . "/akfgfragments/normalise_title.php"); ?>
+<?php require(get_theme_root() . "/akfgfragments/normalise_title.php"); ?>
 
 <main role="main">
     <div class="container">
@@ -13,13 +13,19 @@
                 $results = $mvdb->get_results("SELECT id, title_ro, director, `date` FROM music_videos ORDER BY id ASC;");
 
                 if (!empty($results)) {
-                    foreach ($results as $row) {
-                        echo "<div class='row'>";
-                        echo "<h3><a href='/mv?" . normaliseTitle($row->title_ro) . "' target='_blank'>$row->title_ro</a></h3>";
-                        echo "<p>" . __( 'Year', 'akfgfragments' ) . ": " . date("Y", strtotime("$row->date")) . "</p>";
-                        echo "<p>" . __( 'Director', 'akfgfragments' ) . ": " . $row->director . "</p>";
+                    echo "<div class='row'>";
+                    for ($j = 0; $j < count($results); $j++) {
+                        $title = $results[$j]->title_ro;
+                        $date = $results[$j]->date;
+                        $dir = $results[$j]->director;
+                        echo "<div class='col-lg-4 col-sm-6'>";
+                        echo "<h3><a href='/mv?" . normaliseTitle($title) . "' target='_blank'>$title</a></h3>";
+                        echo "<p>" . __('Year', 'akfgfragments') . ": " . date("Y", strtotime("$date")) . "</p>";
+                        echo "<p>" . __('Director', 'akfgfragments') . ": " . $dir . "</p>";
                         echo "</div>";
+
                     }
+                    echo "</div>";
                 }
                 ?>
             </div>
