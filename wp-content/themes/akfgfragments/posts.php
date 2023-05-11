ace0f9cc9997f53dd42c2c47e7634257e2bf6532
TimaGribanov <48593815+TimaGribanov@users.noreply.github.com>
2023-05-11 10:30:24 +0200

    #9 Videos in post

--- wp-content/themes/akfgfragments/posts.php
+++ wp-content/themes/akfgfragments/posts.php
@@ -81,35 +81,17 @@ if (is_single() && 'post' == get_post_type()) { ?>
     </div>
 <?php } ?>
 
-<!-- POST ON MOBILE -->
-<div class="row main-post-mobile d-block d-md-block d-lg-none">
-    <a class="main-title-mobile" href="<?php the_permalink(); ?>">
-        <h1><?php the_title(); ?></h1>
-    </a>
-    <h6><?php
-    $date_format = get_option('date_format');
-    $time_format = get_option('time_format');
-    $timezone = $_COOKIE["local_timezone"];
-    echo wp_date("{$date_format} {$time_format}", get_post_timestamp(), new DateTimeZone($timezone));
-    ?></h6>
-    <?php
-    $post_tags = get_the_tags();
-    if (!empty($post_tags)) {
-        echo '<ul class="main-post-taglist">';
-        foreach ($post_tags as $post_tag) {
-            echo '<li><a href="' . get_tag_link($post_tag) . '" class="main-tag-mobile">' . $post_tag->name . '</a></li>';
+<script type="text/javascript">
+    window.onload = () => {
+        const ytVideos = document.getElementsByClassName('wp-block-embed-youtube')
+
+        console.log(ytVideos)
+
+        for (let i = 0; i < ytVideos.length; i++) {
+            const e = ytVideos[i]
+
+            e.className = ''
+            e.firstChild.className = 'ratio ratio-16x9'
         }
-        echo '</ul>';
     }
-    ?>
-    <?php
-    if (has_post_thumbnail()) {
-        $post_thumbnail_id = get_post_thumbnail_id();
-        $post_thumbnail_url = wp_get_attachment_url($post_thumbnail_id);
-        echo '<div class="post-image"><a href="';
-        the_permalink();
-        echo '" class="main-thumbnail-mobile"><img title="image title" alt="thumb image" class="wp-post-image" src="' . $post_thumbnail_url . '" style="width:100%; height:auto;"></a></div>';
-    }
-    ?>
-    <p><?php the_content(__('(more...)', 'akfgfragments')); ?></p>
-</div>
+</script>
