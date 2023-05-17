93197792f66de8c7d3aaa0007feb05a3b607f57d
Timofey Gribanov <48593815+TimaGribanov@users.noreply.github.com>
2023-05-17 15:13:56 +0200

    Update mv.php

--- wp-content/themes/akfgfragments/templates/mv.php
+++ wp-content/themes/akfgfragments/templates/mv.php
@@ -2,6 +2,8 @@
 
 <?php get_header(); ?>
 
+<?php require(get_theme_root() . "/akfgfragments/get_people.php"); ?>
+
 <main role="main">
     <div class="container">
         <div id="main" class="row">
@@ -11,6 +13,7 @@
                 require_once(get_theme_root() . "/akfgfragments/normalise_title.php");
                 $mvdb = new wpdb(DATA_DB_USER, DATA_DB_PWD, DATA_DB_NAME, DATA_DB_HOST);
                 $results = $mvdb->get_results("SELECT * FROM music_videos WHERE title_ro = \"$title_parsed\";");
+                $curr_locale = get_locale();
 
                 if (!empty($results)) {
                     foreach ($results as $row) {
@@ -24,7 +27,11 @@
                             <source src='$row->url' type='video/mp4'>
                             </video>";
                         }
-                        echo "<p class='mt-3 mb-1'><strong>" . __( 'Director', 'akfgfragments' ) . ":</strong> $row->director</p>";
+
+                        $dir_id = $row->director;
+                        $dir = getName($dir_id, $curr_locale);
+
+                        echo "<p class='mt-3 mb-1'><strong>" . __( 'Director', 'akfgfragments' ) . ":</strong> $dir</p>";
                         echo "<p class='mt-1 mb-1'><strong>" . __( 'Year', 'akfgfragments' ) . ":</strong> " . date("Y", strtotime("$row->date")) . "</p>";
                         echo "<p class='mt-1'><strong><a class='mv-link' href='/song?" . normaliseTitle($title_parsed) . "' target='_blank'>Info about the song</a></strong></p>";
                     }
