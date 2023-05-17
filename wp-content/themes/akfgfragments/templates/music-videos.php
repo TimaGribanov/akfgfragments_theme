7675be459fb4deb5b0d6aa611dc843e6cb889ed4
Timofey Gribanov <48593815+TimaGribanov@users.noreply.github.com>
2023-05-17 15:16:16 +0200

    Update music-videos.php

--- wp-content/themes/akfgfragments/templates/music-videos.php
+++ wp-content/themes/akfgfragments/templates/music-videos.php
@@ -3,22 +3,7 @@
 <?php get_header(); ?>
 
 <?php require_once(get_theme_root() . "/akfgfragments/normalise_title.php"); ?>
-
-<?php
-function getName($input_id, $locale) {
-    $output_name = "";
-    $locale_parsed = substr($locale, 0, 2);
-
-    $namedb = new wpdb(DATA_DB_USER, DATA_DB_PWD, DATA_DB_NAME, DATA_DB_HOST);
-    $results = $namedb->get_results("SELECT id, $locale_parsed as `name` FROM personnel WHERE id = $input_id;");
-
-    foreach ($results as $row) {
-        $output_name = $row->name;
-    }
-
-    return $output_name;
-}
-?>
+<?php require(get_theme_root() . "/akfgfragments/get_people.php"); ?>
 
 <main role="main">
     <div class="container">
