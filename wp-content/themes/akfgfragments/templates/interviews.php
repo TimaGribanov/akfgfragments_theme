faca14cd68c62ee1c9040ff128f92bd1fa92407c
Timofey Gribanov <48593815+TimaGribanov@users.noreply.github.com>
2023-05-18 15:14:30 +0200

    Revert "Testbed"

--- wp-content/themes/akfgfragments/templates/interviews.php
+++ wp-content/themes/akfgfragments/templates/interviews.php
@@ -11,63 +11,22 @@
                 <?php
                 $curr_locale = get_locale();
                 $locale = substr($curr_locale, 0, 2);
+                $intdb = new wpdb(DATA_DB_USER, DATA_DB_PWD, DATA_DB_NAME, DATA_DB_HOST);
+                $results = $intdb->get_results("SELECT i.slug AS `slug`, i.date AS `date`, it.title AS `title` FROM interviews i JOIN interviews_text it ON i.id = it.interview_id WHERE it.lang = \"$locale\" ORDER BY i.date;");
 
-                echo "<div class='dropdown'>";
-                echo "<button class='btn btn-langs dropdown-toggle' type='button' data-bs-toggle='dropdown' aria-expanded='false'>Change interviews' language</button>";
-                echo "<ul class='dropdown-menu dropdown-menu-end'>";
-                echo "<li class='dropdown-item lang-list' lang='en'>";
-                _e('English', 'akfgfragments');
-                echo "</li>";
-                echo "<li class='dropdown-item lang-list' lang='de'>";
-                _e('German', 'akfgfragments');
-                echo "</li>";
-                echo "<li class='dropdown-item lang-list' lang='es'>";
-                _e('Spanish', 'akfgfragments');
-                echo "</li>";
-                echo "<li class='dropdown-item lang-list' lang='fr'>";
-                _e('French', 'akfgfragments');
-                echo "</li>";
-                echo "<li class='dropdown-item lang-list' lang='ru'>";
-                _e('Russian', 'akfgfragments');
-                echo "</li>";
-                // echo "<li class='dropdown-item lang-list' lang='uk'>";
-                // _e('Ukrainian', 'akfgfragments');
-                // echo "</li>";
-                // echo "<li class='dropdown-item lang-list' lang='be'>";
-                // _e('Belarusian', 'akfgfragments');
-                // echo "</li>";
-                echo "</ul>";
-                echo "</div>";
-
-                echo "<div id='interviews-list'></div>";
+                if (!empty($results)) {
+                    foreach ($results as $row) {
+                        echo "<div class='row'>";
+                        echo "<h3><a class='interviews-links' href='/interview?slug=" . $row->slug . "&lang=" . $locale . "' target='_blank'>$row->title</a></h3>";
+                        echo "</div>";
+                    }
+                }
                 ?>
             </div>
             <?php get_sidebar(); ?>
         </div>
     </div>
 </main>
-
-<script type="text/javascript">
-    //load interviews
-    $('.lang-list').click(function () {
-        const lang = $(this).attr('lang')
-        $.ajax({
-            url: `/wp-content/themes/akfgfragments/get_interviews.php?slug=&lang=${lang}`,
-            success: function (response) {
-                $('#interviews-list').html(response)
-            }
-        })
-    })
-
-    $(function () {
-        $.ajax({
-            url: `/wp-content/themes/akfgfragments/get_interviews.php?slug=&lang=<?php global $locale; echo $locale; ?>`,
-            success: function (response) {
-                $('#interviews-list').html(response)
-            }
-        })
-    })
-</script>
 
 <?php get_footer(); ?>
 
