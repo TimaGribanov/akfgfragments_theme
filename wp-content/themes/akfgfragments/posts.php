8afa70acc21680bf7172014e49d616f9a0e55ac6
TimaGribanov <48593815+TimaGribanov@users.noreply.github.com>
2023-07-03 11:26:00 +0200

    mobile feed fixed; version up

--- wp-content/themes/akfgfragments/posts.php
+++ wp-content/themes/akfgfragments/posts.php
@@ -57,6 +57,7 @@ if (is_single() && 'interview' == get_post_type()) { ?>
     <?php
     // FOR FEED
 } else { ?>
+        <!-- DESKTOP -->
         <div class="row main-post d-none d-lg-block d-xl-block d-xxl-block">
             <div class="row">
                 <div class="col-4">
@@ -75,6 +76,49 @@ if (is_single() && 'interview' == get_post_type()) { ?>
                     ?>
                 </div>
                 <div class="col-8 main-post-right">
+                    <a href="<?php the_permalink(); ?>">
+                        <h1><?php the_title(); ?></h1>
+                    </a>
+                    <h6><?php
+                    $date_format = get_option('date_format');
+                    $time_format = get_option('time_format');
+                    $timezone = $_COOKIE["local_timezone"];
+                    echo wp_date("{$date_format} {$time_format}", get_post_timestamp(), new DateTimeZone($timezone));
+                    ?></h6>
+                    <?php
+                    $post_tags = get_the_tags();
+                    if (!empty($post_tags)) {
+                        echo '<ul class="main-post-taglist">';
+                        foreach ($post_tags as $post_tag) {
+                            echo '<li><a href="' . get_tag_link($post_tag) . '">' . $post_tag->name . '</a></li>';
+                        }
+                        echo '</ul>';
+                    }
+                    ?>
+                    <p><?php the_content(__('(more...)', 'akfgfragments')); ?></p>
+                </div>
+            </div>
+        </div>
+
+        <!-- MOBILE -->
+        <div class="row main-post d-block d-sm-block d-md-block">
+            <div class="row">
+                <div class="row">
+                    <?php
+                    if (has_post_thumbnail()) {
+                        $post_thumbnail_id = get_post_thumbnail_id();
+                        $post_thumbnail_url = wp_get_attachment_url($post_thumbnail_id);
+                        echo '<div class="post-image"><a href="';
+                        the_permalink();
+                        echo '" class="main-thumbnail"><img title="';
+                        the_title();
+                        echo '" alt="';
+                        the_title();
+                        echo '" class="wp-post-image" src="' . $post_thumbnail_url . '"></a></div>';
+                    }
+                    ?>
+                </div>
+                <div class="row main-post-right">
                     <a href="<?php the_permalink(); ?>">
                         <h1><?php the_title(); ?></h1>
                     </a>
