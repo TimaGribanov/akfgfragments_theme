129693a1970884cf5aa8ff831df5a6d0d01ab3d3
Tima Gribanov <48593815+TimaGribanov@users.noreply.github.com>
2024-05-10 16:20:23 +0200

    Update release.php

--- wp-content/themes/akfgfragments/templates/release.php
+++ wp-content/themes/akfgfragments/templates/release.php
@@ -56,7 +56,7 @@ function printReleaseDate($date)
     if ($date == "2000-01-01" || $date == "2002-01-01") {
         dateBlock("short", $date);
     } else if ($release->date == "0000-00-00") {
-        dateBlock("unknown", $date);
+        unknownDateBlock();
     } else {
         dateBlock("full", $date);
     }
